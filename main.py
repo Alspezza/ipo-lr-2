7e28dac487a0f3cecaeac944ec892a91b1892c53
alpsezza <zspecian23@gmail.com>
2025-09-22 14:31:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+var_int=70
+var_float=8.4
+var_str="No"
+var_str= var_str * 3 + "YesYes"
+big_int35= var_int * 3.5
+var_float = var_float-1
+
+print("биг инт=", big_int35 , 
+       "вар флоат=", var_float ,
+      "инт / флоат=",   var_int / var_float      ,
+      "биг инт / флоат=", big_int35 / var_float    ,
+    "стр=", var_str)
